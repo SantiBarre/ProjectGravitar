ab6727b32cc31541e08aa0cb5e4ad0aee8982251
SantiBarre <santibarre022@gmail.com>
2022-07-22 21:00:33 -0300

    Iteradores en listas

--- lista.h
+++ lista.h
@@ -5,13 +5,33 @@
 #include <stddef.h>
 
 typedef struct lista lista_t;
+typedef struct lista_iterador lista_iterador_t;
+
 
 lista_t *lista_crear(void);
 
-bool lista_agregar_al_final(lista_t *, void *);
+bool lista_agregar(lista_t *l, void *dato);
 
-size_t lista_largo(lista_t *);
+bool lista_agregar_al_final(lista_t *l, void *dato);
 
-void lista_destruir(lista_t *, void (*destruir_dato)(void *));
+size_t lista_largo(lista_t *l);
+
+void lista_destruir(lista_t *l, void (*destruir_dato)(void *));
+
+lista_t *lista_filtrar(lista_t *l, bool (*f)(void *dato, void *extra), void *extra);
+
+lista_iterador_t *lista_iterador_crear(lista_t *l);
+
+void lista_iterador_destruir(lista_iterador_t *li);
+
+void *lista_iterador_actual(const lista_iterador_t *li);
+
+bool lista_iterador_siguiente(lista_iterador_t *li);
+
+bool lista_iterador_termino(const lista_iterador_t *li);
+
+void *lista_iterador_eliminar(lista_iterador_t *li);
+
+bool lista_iterador_insertar(lista_iterador_t *li, void *dato);
 
 #endif /* LISTA_H */
